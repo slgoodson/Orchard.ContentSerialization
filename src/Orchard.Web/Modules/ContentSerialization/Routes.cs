d6b5bb31a7e546aa6afb3cfa43948f2a57de3b55
Tom Pratt <tinglybill@outlook.com>
2015-08-08 18:00:35 +0100

    Added Raven.Api module as ContentSerialization into the Modules folder

---
+++ src/Orchard.Web/Modules/ContentSerialization/Routes.cs
@@ -0,0 +1,32 @@
+﻿using Orchard.Mvc.Routes;
+using Orchard.WebApi.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ContentSerialization {
+    public class HttpRoutes : IHttpRouteProvider {
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes()) {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[] {
+                new HttpRouteDescriptor {
+                    Priority = 5,
+                    RouteTemplate = "api/Contents/{controller}/{action}/{id}",
+                    Defaults = new {
+                        area = "ContentSerialization",
+                        id = RouteParameter.Optional
+                    }
+                }
+            };
+        }
+    }
+}
